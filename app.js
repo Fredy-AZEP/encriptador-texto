29107601f6a11bcd53377c5011748a7d90ce9a22
Alfredo <alfredozepeta@gmail.com>
2024-07-02 23:05:06 -0600

    Metodo encriptar y desencriptar implementados

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
 function updateText(){
     var inputText = document.getElementById("inputText").value;
-    document.getElementById("outputText").textContent = inputText || "Esperando mensaje";
+
+    var replacements = {
+        'a': 'ai',
+        'e': 'enter',
+        'i': 'imes',
+        'o': 'ober',
+        'u': 'ufat'
+    };
+
+    var encryptedText = inputText.replace(/[aeiou]/g, function(match){
+        return replacements[match.toLowerCase()] || match;
+    });
+
+    document.getElementById("outputText").textContent = encryptedText || "Esperando mensaje";
+}
+
+function decryptText() {
+    var inputText = document.getElementById("inputText").value;
+
+    // Invertir el mapa de reemplazos para desencriptar
+    var replacements = {
+        'ai': 'a',
+        'enter': 'e',
+        'imes': 'i',
+        'ober': 'o',
+        'ufat': 'u'
+    };
+
+    // Función para reemplazar cada secuencia encriptada según el mapa
+    var decryptedText = inputText.replace(/(ai|enter|imes|ober|ufat)/g, function(match) {
+        return replacements[match] || match;
+    });
+
+    document.getElementById("outputText").textContent = decryptedText || "Ningún mensaje fue encontrado";
 }
